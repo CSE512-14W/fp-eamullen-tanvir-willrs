c6e4a1695b600e0ac3fd0903094e7dcadb01ca22
Eric Mullen <emullen@meowth.(none)>
2014-02-09 20:31:14 -0800

    take a look at this

--- vis.js
+++ vis.js
@@ -1,9 +1,47 @@
-//here we'll import the real data
-var dataset = [1,2,3,4,5,6];
+//change to something else when we deploy
+//for now run by opening a terminal and typing
+//"python -m SimpleHTTPServer" in the project directory
+var host = "http://localhost:8000/"
 
-//here we'll eventually make a table, with appliance graphics
-d3.select("body").selectAll("p")
-    .data(dataset)
-    .enter()
-    .append("p")
-    .text(function(x) {return ("The data is: " + x);});
+var h1 = d3.map()
+var h2 = d3.map()
+var h3 = d3.map()
+var h4 = d3.map()
+
+//callback function that runs when all data is loaded
+function ready() {
+    var current = h1;
+
+    //grab the body tag from the DOM, select all paragraphs
+    d3.select("body").selectAll("p")
+    //set data as keys from h1 (house 1 data)
+	.data(current.keys())
+	.enter()
+	.append("p")
+	.text(function(x) {
+	    var a = current.get(x)
+	    return ("Appliance: " + a.name + " was on for: "
+				   + ((a.time_off - a.time_on)/60.0) + " minutes.") })
+}
+
+var i = 0;
+
+//asynchronously load data files, and call ready when finished
+queue()
+    .defer(d3.csv, host + "data/H1.csv", function(d) { 
+	h1.set(i++, {id: +d.id, name: d.Name, time_on: +d.time_on, time_off: +d.time_off}); 
+    })
+    .defer(d3.csv, host + "data/H2.csv", function(d) { 
+	h2.set(i++, {id: +d.id, name: d.Name, time_on: +d.time_on, time_off: +d.time_off}); 
+    })
+    .defer(d3.csv, host + "data/H3.csv", function(d) { 
+	h3.set(i++, {id: +d.id, name: d.Name, time_on: +d.time_on, time_off: +d.time_off}); 
+    })
+    .defer(d3.csv, host + "data/H4.csv", function(d) { 
+	h4.set(i++, {id: +d.id, name: d.Name, time_on: +d.time_on, time_off: +d.time_off}); 
+    })
+    .await(ready)
+
+
+
+
