00e32ef8390f46ecee8a5aa1dfd202e85f111e75
Will Scott <willscott@gmail.com>
2014-03-05 11:50:31 -0800

    start on data relay class

---
+++ server/data.py
@@ -0,0 +1,41 @@
+import os, sys, inspect, time, math
+import threading, datetime
+
+class Data():
+  def __init__(self):
+    self.sources = dict()
+    self.watchers = dict()
+  
+  def run(self):
+    if not next in self:
+      self.next = time.time()
+    self.emit()
+    self.next += 1
+    threading.Timer(next - time.time(), self.run).start()
+
+  def emit(self):
+    for thing in self.sources:
+      line = self.sources[thing].readline()
+      #TODO: deal with running off end / repeating day
+      for cb in self.watchers[thing]:
+        cb(thing, line.split(" ")[1])
+
+  def track(self, thing, cb):
+    try:
+      (house, channel) = thing.split(".")
+      if thing not in self.sources and os.path.exists("data/house_" + house + "/channel_" + channel + ".dat"):
+        self.sources[thing] = open("data/house_" + house + "/channel_" + channel + ".dat", "r")
+      #TODO: jump to requested day & time.
+      if thing not in self.watchers:
+        self.watchers[thing] = []
+      self.watchers[thing].append(cb)
+    except:
+      pass
+
+  def clean(self, cb):
+    for thing in self.watchers:
+      self.watchers[thing].remove(cb)
+      if len(self.watchers[thing]) == 0:
+        self.sources[thing].close()
+        del self.sources[thing]
+        del self.watchers[thing]
